3804df24207bd84fad6f707635903f7873087629
Ubuntu <ubuntu@ip-172-31-34-23.ap-southeast-4.compute.internal>
2025-11-12 08:27:49 +0000

    compiler errors

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -64,15 +64,61 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
         buffer=nl.hbm,
     )
 
-    # Various tiling dimensions (You may want to define more of them)
-    c_in_pmax = nl.tile_size.pmax
-    n_tiles_c_in = in_channels // c_in_pmax
+    X_out_re = nl.ndarray(
+        shape=(batch_size, out_channels, out_height * out_width),
+        dtype=X.dtype,
+        buffer=nl.hbm,
+    )
 
-    # Process the images in batches
+    # tiles for matmul
+    TILE_M = nl.tile_size.gemm_stationary_fmax #128
+    TILE_N = nl.tile_size.gemm_moving_fmax #512
+    TILE_K = nl.tile_size.pmax #128
+
+    M = out_channels
+    N = out_height * out_width
+    K = in_channels
+
     for b in nl.affine_range(batch_size):
-        raise RuntimeError("Please fill your implementation of computing convolution"
-                           " of X[b] with the weights W and bias b, followed by a"
-                           " maxpool and store the result in X_out[b]")
+        for m in nl.affine_range(M // TILE_M):
+            for n in nl.affine_range(N // TILE_N):
+                nl.store(dst=X_out_re[b, m*TILE_M : (m+1)*TILE_M, n*TILE_N:(n+1)*TILE_N], value=0);
 
+    X_out_interm = nl.ndarray(
+        shape=(TILE_M, TILE_N),
+        dtype=X.dtype,
+        buffer=nl.hbm,
+    )
+
+    # process the images in batches
+    for b in nl.affine_range(batch_size):
+        for i in nl.affine_range(filter_height):
+            for j in nl.affine_range(filter_width):
+                # get the part of the input we care about for the pixel i,j in our filter
+                input_chopped = nl.ndarray(shape=(in_channels, out_height, out_width), dtype=X.dtype, buffer=nl.sbuf)
+                nisa.dma_copy(dst=input_chopped, src=X[b, :, i:i+out_height, j:j+out_width])
+                input_chopped = input_chopped.reshape((in_channels, out_height*out_width))
+                for m in nl.affine_range(M // TILE_M):
+                    for n in nl.affine_range(N // TILE_N):  # if our last tile is slightly empty, we'll do some extra math
+                        res_psum = nl.zeros((TILE_M, TILE_N), dtype=nl.float32, buffer=nl.psum)
+                        for k in nl.affine_range(K // TILE_K):
+                            # declare tiles in the SBUF
+                            filter_tile = nl.ndarray((TILE_M, TILE_K), dtype=W.dtype, buffer=nl.sbuf)
+                            filter_tile_T = nl.ndarray((TILE_K, TILE_M), dtype=W.dtype, buffer=nl.sbuf)
+                            input_tile = nl.ndarray((TILE_K, TILE_N), dtype=X.dtype, buffer=nl.sbuf)
+
+                            # load data into our tiles
+                            nisa.dma_copy(dst=filter_tile, src=W[m*TILE_M : (m+1)*TILE_M, k*TILE_K : (k+1)*TILE_K, i, j])
+                            fl_tile_interm = nisa.nc_transpose(filter_tile)
+                            filter_tile_T = nisa.tensor_copy(fl_tile_interm, engine=nisa.vector_engine)
+                            nisa.dma_copy(dst=input_tile, src=input_chopped[k*TILE_K : (k+1)*TILE_K, n*TILE_N : (n+1)*TILE_N])
+
+                            #accumulate partial sums into PSUM
+                            res_psum += nisa.nc_matmul(filter_tile_T[...], input_tile[...])
+
+                        res_sb = nl.copy(res_psum, dtype=X_out.dtype)
+                        nisa.dma_copy(dst=X_out_interm, src=res_sb)
+                        X_out_re[b, m*TILE_M : (m+1)*TILE_M, n*TILE_N : (n+1)*tile_N] += res_sb 
+    X_out_re = X_out_re.reshape((batch_size, out_channels, out_pool_height, out_pool_width))
+    nki.tensor.store(X_out_re, X_out)
     return X_out
-
